b01c26652a45790859b41e71d8eb7cdd088b4c8c
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-12-03 21:20:32 +0100

    create navbar styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
+import '../scss/main.scss';
+import '../scss/_login.scss';
+
 const Login = () => (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/logowanie">Zaloguj</Link>
-                </li>
-                <li>
-                    <Link to="/rejestracja">Załóż konto</Link>
-                </li>
-            </ul>
+        <nav className="loginbar">
+            <div className="login-container">
+                <ul>
+                    <li className="login-item">
+                        <Link to="/logowanie" className="log-links">Zaloguj</Link>
+                    </li>
+                    <li className="login-item">
+                        <Link to="/rejestracja" className="log-links">Załóż konto</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
 );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HomeFourSteps from "./HomeFourSteps";
+
+import '../scss/main.scss';
+import '../scss/_navbar.scss';
 
 const Navbar = () => (
-        <nav>
-                <ul>
-                        <li>
-                                <Link to="/">Start</Link>
-                        </li>
-                        <li>
-                                <Link to="/ocochodzi">O co chodzi?</Link>
-                        </li>
-                        <li>
-                                <Link to="/onas">O nas</Link>
-                        </li>
-                        <li>
-                                <Link to="/partnerzy">Fundacja i organizacje</Link>
-                        </li>
-                        <li>
-                                <Link to="/kontakt">Kontakt</Link>
-                        </li>
-                </ul>          
+        <nav className="navbar">
+                <div className="navbar-container">
+                        <ul>
+                                <li className="nav-item">
+                                        <Link to="/" className="nav-links">Start</Link>
+                                </li>
+                                <li className="nav-item">
+                                        <Link to="/ocochodzi" className="nav-links" element={<HomeFourSteps />} >O co chodzi?</Link>
+                                </li>
+                                <li className="nav-item">
+                                        <Link to="/onas" className="nav-links">O nas</Link>
+                                </li>
+                                <li className="nav-item">
+                                        <Link to="/partnerzy" className="nav-links">Fundacja i organizacje</Link>
+                                </li>
+                                <li className="nav-item">
+                                        <Link to="/kontakt" className="nav-links">Kontakt</Link>
+                                </li>
+                        </ul>
+                </div>          
         </nav>
 );
 
