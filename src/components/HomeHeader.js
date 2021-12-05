bc8281f95b58b3da22929f6714a51b388f40d4fe
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-12-05 12:28:02 +0100

    create header content

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Decoration from '../assets/assets/assets/Decoration.svg';
 
 const HomeHeader = () => (
-    <div>
-            HomeHeader
+    <div className="homeheader">
+            <div className="header-content">
+                <h2>Zacznij pomagać! <br />Oddaj niechciane rzeczy w zaufane ręce</h2>
+                <div><img src={Decoration} /></div>
+                <div className="header__buttons">
+                    <div className="header-button">
+                        <Link to="/logowanie">Oddaj rzeczy</Link>
+                    </div>
+                    <div className="header-button">
+                        <Link to="/logowanie">Zorganizuj zbiórkę</Link>
+                    </div>
+                </div>
+            </div>
     </div>
 );
 
