1571dfb48cce6a0b4844aa2eab69a4173fa1fcca
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-12-05 19:17:49 +0100

    create three columns component and styles

--- src/components/HomeFourSteps.js
+++ src/components/HomeFourSteps.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Decoration from '../assets/assets/assets/Decoration.svg';
+import IconFirst from '../assets/assets/assets/Icon-1.svg';
+import IconSecond from '../assets/assets/assets/Icon-2.svg';
+import IconThird from '../assets/assets/assets/Icon-3.svg';
+import IconFourth from '../assets/assets/assets/Icon-4.svg';
+
+import '../scss/_homefoursteps.scss';
 
 const HomeFourSteps = () => (
-    <div>
-            HomeFourSteps
+    <div className="homefoursteps">
+        <div className="steps__title">
+            <p>Wystarczą 4 proste kroki</p>
+            <div><img src={Decoration} /></div>
+        </div>
+        <div className="steps__icons">
+            <div className="icons-container">
+                <div className="column">
+                    <div className="icon"><img src={IconFirst} /></div>
+                    <p className="title">Wybierz rzeczy</p>
+                    <p className="content">ubrania, zabawki, sprzęt i inne</p>
+                </div>
+                <div className="column">
+                    <div className="icon"><img src={IconSecond} /></div>
+                    <p className="title">Spakuj je</p>
+                    <p className="content">skorzystaj z worków na śmieci</p>
+                </div>
+                <div className="column">
+                    <div className="icon"><img src={IconThird} /></div>
+                    <p className="title">Zdecyduj, komu chcesz pomóc</p>
+                    <p className="content">wybierz zaufane miejsce</p>
+                </div>
+                <div className="column">
+                    <div className="icon"><img src={IconFourth} /></div>
+                    <p className="title">Zamów kuriera</p>
+                    <p className="content">kurier przyjedzie w dogodnym terminie</p>
+                </div>
+            </div>
+        </div>
+        <div className="steps__button">
+            <div className="">
+                <Link to="/logowanie" className="head-button">Oddaj <br />rzeczy</Link>
+            </div>
+        </div>
     </div>
 );
 
--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -5,20 +5,22 @@ import '../scss/_homethreecolumns.scss';
 const HomeThreeColumns = () => (
     <div className="homethreecolumns">
             <div className="threecolumns-container">
-                <div className="column">
-                    <p className="column_number">10</p>
-                    <p className="column_title">oddanych worków</p>
-                    <p className="column_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                <div className="column">
-                    <p className="column_number">5</p>
-                    <p className="column_title">wspartych organizacji</p>
-                    <p className="column_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                <div className="column">
-                    <p className="column_number">7</p>
-                    <p className="column_title">zorganizowanych zbiórek</p>
-                    <p className="column_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div>
+                    <div className="column">
+                        <p className="column_number">10</p>
+                        <p className="column_title">oddanych worków</p>
+                        <p className="column_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    </div>
+                    <div className="column">
+                        <p className="column_number">5</p>
+                        <p className="column_title">wspartych organizacji</p>
+                        <p className="column_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    </div>
+                    <div className="column">
+                        <p className="column_number">7</p>
+                        <p className="column_title">zorganizowanych zbiórek</p>
+                        <p className="column_content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    </div>
                 </div>
             </div>
     </div>
