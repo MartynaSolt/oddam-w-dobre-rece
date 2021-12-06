7aa5a169e856bf4a155d92cf37c10bde9e0ec100
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-12-06 20:38:51 +0100

    create about us section styles

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -9,10 +9,12 @@ const HomeAboutUs = () => (
     <div className="homeaboutus">
         <div className="aboutus_section">
             <div className="aboutus_content">
-                <p className="aboutus-title">O nas</p>
-                <div><img src={Decoration} /></div>
-                <p className="aboutus-story">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                <div><img src={Signature} /></div>
+                <div className="content-container">
+                    <p className="aboutus-title">O nas</p>
+                    <div><img src={Decoration} /></div>
+                    <p className="aboutus-story">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                    <div className="signature"><img src={Signature} /></div>
+                </div>
             </div>
             <div className="aboutus-photo"></div>
         </div>
