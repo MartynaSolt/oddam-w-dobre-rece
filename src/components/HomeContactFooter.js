4edbe8732a6fed6306e5d4437b458b25cf4ec013
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-12-16 17:17:28 +0100

    add contact styles

--- src/components/HomeContactFooter.js
+++ src/components/HomeContactFooter.js
@@ -11,35 +11,37 @@ const HomeContactFooter = () => (
         <div className="opacity">
             <div className="contactfooter-container">
                 <div className="contact">
-                    <p>Skontaktuj się z nami!</p>
-                    <div><img src={Decoration} /></div>
-                    <div className="form">
-                        <form>
-                            <div className="form_container">
-                                <div className="form_name-email">
-                                    <div className="form_name">
-                                        <label htmlFor="name">Wpisz swoje imię</label>
-                                        <input 
-                                            type="text" 
-                                            id="name">
-                                        </input>
+                    <div className="contact_container">
+                        <p>Skontaktuj się z nami!</p>
+                        <div className="decoration"><img src={Decoration} /></div>
+                        <div className="form">
+                            <form>
+                                <div className="form_container">
+                                    <div className="form_name-email">
+                                        <div className="form_name">
+                                            <label htmlFor="name">Wpisz swoje imię</label>
+                                            <input 
+                                                type="text" 
+                                                id="name">
+                                            </input>
+                                        </div>
+                                        <div className="form_email">
+                                            <label htmlFor="email">Wpisz swój email</label>
+                                            <input 
+                                                type="text" 
+                                                id="email">
+                                            </input>
+                                        </div>
                                     </div>
-                                    <div className="form_email">
-                                        <label htmlFor="email">Wpisz swój email</label>
-                                        <input 
-                                            type="text" 
-                                            id="email">
-                                        </input>
+                                    <div className="form_message">
+                                        <label htmlFor="message">Wpisz swoją wiadomość</label>
+                                        <textarea  
+                                            id="message">
+                                        </textarea>
                                     </div>
                                 </div>
-                                <div className="form_message">
-                                    <label htmlFor="message">Wpisz swoją wiadomość</label>
-                                    <textarea  
-                                        id="message">
-                                    </textarea>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <div className="footer">
