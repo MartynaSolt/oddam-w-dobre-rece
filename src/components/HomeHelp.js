0b2fa44817fcf7d23831e9bacf88234aca642e6a
MartynaSolt <83539177+MartynaSolt@users.noreply.github.com>
2021-12-18 19:48:23 +0100

    add some logic to help component

--- src/components/HomeHelp.js
+++ src/components/HomeHelp.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import JsonDataFundations from '../FUNDACJE_DATA.json';
+import JsonDataOrganizations from '../ORGANIZACJE_DATA.json';
+import JsonDataFundraising from '../FUNDACJE_DATA.json';
+
 import ReactPaginate from 'react-paginate';
 
 import Decoration from '../assets/assets/assets/Decoration.svg';
@@ -8,6 +11,9 @@ import '../scss/_homehelp.scss';
 
 const HomeHelp = () => {
     const [funds, setFunds] =  useState(JsonDataFundations.slice(0, 9));
+    const [org, setOrg] = useState(JsonDataOrganizations.slice(0, 6));
+    const [raising, setRaising] = useState(JsonDataFundraising.slice(0, 3));
+    
     const [pageNumber, setPageNumber] = useState(0);
 
     const fundsPerPage = 3
@@ -30,6 +36,42 @@ const HomeHelp = () => {
                 </div>
             </div>
         ));
+
+    const displayOrg = org
+        .slice(pagesVisited, pagesVisited + fundsPerPage)
+        .map((org) => (
+            <div className="content">
+                <div className="content_list">
+                    <div className="content_item">
+                        <div className="top">
+                            <p className="name">{org.name}</p>
+                            <p className="things">{org.things}</p>
+                        </div>
+                        <div className="bottom">
+                            <p className="idea">{org.idea}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ));
+        
+        const displayRaising = raising
+        .slice(pagesVisited, pagesVisited + fundsPerPage)
+        .map((raising) => (
+            <div className="content">
+                <div className="content_list">
+                    <div className="content_item">
+                        <div className="top">
+                            <p className="name">{raising.name}</p>
+                            <p className="things">{raising.things}</p>
+                        </div>
+                        <div className="bottom">
+                            <p className="idea">{raising.idea}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ));     
 
     const pageCount = Math.ceil(funds.length / fundsPerPage);
 
